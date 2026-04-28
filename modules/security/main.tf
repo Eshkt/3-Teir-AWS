5213358ed67c7e6fe3bbda2fd2aa04041f276ca2
Franky Parcon <franky.parcon@globe.com.ph>
2026-04-28 15:21:16 +0800

    SEC: Security group

---
+++ modules/security/main.tf
@@ -0,0 +1,37 @@
+#aws security group
+resource "aws_security_group" "web_sg" {
+    name = "${var.project_name}-web-sg"
+    description = "Security group for web servers"
+    vpc_id = var.vpc_id
+
+    ingress = {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"] #open all
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["112.198.36.8/32"] #work ip address only
+    }
+
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"] #open all
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"] #open all
+    }
+    tags = { Name = "${var.project_name}-web-sg" }
+}
+
+#Export the security group id so that it can be used in other modules
---
+++ modules/security/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_id" {
+    description = "Define the value/id of the VPC"
+    type = string
+}
+
+variable "project_name" {
+    type = string
+}
+
