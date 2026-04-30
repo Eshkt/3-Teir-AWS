bdb0b92a7d910c548255a6cc098d145395e457a4
Franky Parcon <franky.parcon@globe.com.ph>
2026-04-30 16:18:48 +0800

    Fixed: Sytanx bugs

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "web_sg" {
     description = "Security group for web servers"
     vpc_id = var.vpc_id
 
-    ingress = {
+    ingress {
         from_port = 80
         to_port = 80
         protocol = "tcp"
